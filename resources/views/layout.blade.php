d2cef24b360a3a0c72a17db6cbc6b70051b46c0a
Lorenzo Cattaneo <info@lorenzocattaneo.com>
2021-01-27 15:29:48 +0100

    fix mix path

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,8 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @livewireStyles
-    <link href="{{mix('drystack/css/components.css')}}" rel="stylesheet">
-    <script src="{{mix('drystack/js/components.js')}}"></script>
+    <link href="{{mix('/css/components.css', '/drystack')}}" rel="stylesheet">
+    <script src="{{mix('/js/components.js', '/drystack')}}" defer></script>
     <script src="https://kit.fontawesome.com/940802e517.js" crossorigin="anonymous"></script>
 </head>
 <body class="h-screen overflow-hidden flex items-center justify-center bg-neutral-100 font-sans">
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,4 +1,4 @@
-<x-nav-link route="home" icon="fas fa-home">Home</x-nav-link>
-<x-nav-group title="Prova" :stacked="$stacked">
-    <x-nav-link route="home" icon="fas fa-home" :stacked="$stacked">Home</x-nav-link>
-</x-nav-group>
+<x-nav-link route="dashboard" icon="fas fa-home">Dashboard</x-nav-link>
+{{--<x-nav-group title="Group title" :stacked="$stacked">--}}
+{{--    <x-nav-link route="Group link" icon="" :stacked="$stacked">Group link</x-nav-link>--}}
+{{--</x-nav-group>--}}
