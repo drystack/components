3dea6a624f12ea2e9954d27f15f9e9be896490a6
Lorenzo Cattaneo <info@lorenzocattaneo.com>
2021-01-27 22:13:34 +0100

    fix buttons styles

--- resources/views/components/button/button.blade.php
+++ resources/views/components/button/button.blade.php
@@ -3,14 +3,14 @@
     'style' => 'full',
     'custom' => null
 ])
-@php($base = "px-6 py-2 shadow rounded m-1 uppercase text-xs tracking-wide ")
+@php($base = "px-6 py-2 rounded m-1 uppercase text-xs tracking-wide ")
 
 @if($style == 'outline')
-    @php($class = $base . " bg-transparent text-$color-500 border border-$color-500 hover:border-$color-700 hover:bg-$color-600 hover:text-white")
+    @php($class = $base . " shadow bg-transparent text-$color-500 border border-$color-500 hover:border-$color-700 hover:bg-$color-600 hover:text-white")
 @elseif($style == 'text')
     @php($class = $base . " bg-transparent text-$color-500 font-normal underline hover:text-$color-600 focus:outline-none")
 @else
-    @php($class = $base . " bg-transparent text-white bg-$color-500 border-$color-500 hover:border-$color-600 hover:bg-$color-600")
+    @php($class = $base . " shadow bg-transparent text-white bg-$color-500 border-$color-500 hover:border-$color-600 hover:bg-$color-600")
 @endif
 
 @if(isset($custom))
