790a3326001e7d3539dc1906e5c4f99fc5363ba3
Lorenzo Cattaneo <info@lorenzocattaneo.com>
2021-01-28 11:52:52 +0100

    fix group open syntax

--- resources/views/components/nav/group.blade.php
+++ resources/views/components/nav/group.blade.php
@@ -19,7 +19,7 @@
             </span>
         </div>
         <div>
-            <svg class="w-6 h-6 float-right" :class="open === true ? 'transform rotate-180' : ''"> fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+            <svg class="w-6 h-6 float-right" :class="open === true ? 'transform rotate-180' : ''" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
         </div>
     </div>
     <div
