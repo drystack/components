34b9df6c5f602e85ca441854fb58fced9d755467
Lorenzo Cattaneo <info@lorenzocattaneo.com>
2021-02-02 21:43:24 +0100

    update grid

--- resources/views/components/layout/grid.blade.php
+++ resources/views/components/layout/grid.blade.php
@@ -1,12 +1,12 @@
 @props([
-    'cols' => '1',
-    'rows' => '1',
-    'gap' => '6'
+'cols' => '1',
+'rows' => '1',
+'gap' => '6'
 ])
 @php($grid_cols_class = 'grid-cols-' . ($cols == 0 ? 'none' : $cols))
 @php($grid_rows_class = 'grid-rows-' . ($rows == 0 ? 'none' : $rows))
 @php($gap_class = 'gap-' . $gap)
 
-<div {{ $attributes->merge(['class' => "$grid_rows_class $grid_cols_class $gap_class"]) }}>
+<div {{ $attributes->merge(['class' => "w-full grid md:$grid_rows_class md:$grid_cols_class grid-cols-1 grid-rows-1 $gap_class"]) }}>
     {{ $slot }}
 </div>
